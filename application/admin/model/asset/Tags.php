40e2195d6e10511bb3f13ed5053643f0b961b250
ethanaicode <cshengs@163.com>
2026-05-22 10:59:29 +0800

    feat: add tags

---
+++ application/admin/model/asset/Tags.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace app\admin\model\asset;
+
+use think\Model;
+
+
+class Tags extends Model
+{
+
+    
+
+    
+
+    // 表名
+    protected $name = 'asset_tags';
+    
+    // 自动写入时间戳字段
+    protected $autoWriteTimestamp = false;
+
+    // 定义时间戳字段名
+    protected $createTime = false;
+    protected $updateTime = false;
+    protected $deleteTime = false;
+
+    // 追加属性
+    protected $append = [
+
+    ];
+    
+
+    
+
+
+
+
+
+
+
+}
---
+++ public/assets/js/backend/asset/tags.js
@@ -0,0 +1,51 @@
+define(['jquery', 'bootstrap', 'backend', 'table', 'form'], function ($, undefined, Backend, Table, Form) {
+
+    var Controller = {
+        index: function () {
+            // 初始化表格参数配置
+            Table.api.init({
+                extend: {
+                    index_url: 'asset/tags/index' + location.search,
+                    add_url: 'asset/tags/add',
+                    edit_url: 'asset/tags/edit',
+                    del_url: 'asset/tags/del',
+                    multi_url: 'asset/tags/multi',
+                    import_url: 'asset/tags/import',
+                    table: 'asset_tags',
+                }
+            });
+
+            var table = $("#table");
+
+            // 初始化表格
+            table.bootstrapTable({
+                url: $.fn.bootstrapTable.defaults.extend.index_url,
+                pk: 'id',
+                sortName: 'id',
+                columns: [
+                    [
+                        {checkbox: true},
+                        {field: 'id', title: __('Id')},
+                        {field: 'name', title: __('Name'), operate: 'LIKE'},
+                        {field: 'operate', title: __('Operate'), table: table, events: Table.api.events.operate, formatter: Table.api.formatter.operate}
+                    ]
+                ]
+            });
+
+            // 为表格绑定事件
+            Table.api.bindevent(table);
+        },
+        add: function () {
+            Controller.api.bindevent();
+        },
+        edit: function () {
+            Controller.api.bindevent();
+        },
+        api: {
+            bindevent: function () {
+                Form.api.bindevent($("form[role=form]"));
+            }
+        }
+    };
+    return Controller;
+});
